e410cba9db3f111ba82945e494a13502474dcf6a
Laysa Dias <111162126+LaysaDias@users.noreply.github.com>
2024-03-21 20:43:21 -0300

    update

--- api-fatec/api-fatec/src/main/java/br/com/api/fatec/apifatec/entities/Cliente.java
+++ api-fatec/api-fatec/src/main/java/br/com/api/fatec/apifatec/entities/Cliente.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Email;
+import javax.validation.constraints.Email;
 
 @Entity
 @Table(name = "clientes")
@@ -15,17 +15,61 @@ public class Cliente {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@Column(nullable = false, length = 60)
 	private String nome;
-	
+
 	@Column(name = "razao_social", length = 60, nullable = false)
 	private String razaoSocial;
-	
+
 	@Column()
 	private String endereco;
-	
+
 	@Column()
-	@Email 
+	@Email
 	private String email;
+
+	
+	
+	
+	// Getters e setters
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
